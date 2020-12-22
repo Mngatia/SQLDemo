f26df273a670e0ddceb2c9ceba5343a641f1f747
Mngatia <nyawirahngatia@gmail.com>
2020-12-22 21:30:01 +0300

    Actual Listeners for each button click.

--- app/src/main/java/com/example/sqldemo/MainActivity.java
+++ app/src/main/java/com/example/sqldemo/MainActivity.java
@@ -3,12 +3,59 @@ package com.example.sqldemo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Switch;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    //reference to buttons and other controls on the layout
+
+    Button bt_add, btn_viewAll;
+    EditText et_name, et_age;
+    Switch sw_activeCustomer;
+    ListView lv_customerList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //assigned a value here
+
+        bt_add = findViewById(R.id.btn_add);
+        btn_viewAll = findViewById(R.id.btn_viewAll);
+        et_age = findViewById(R.id.et_age);
+        et_name = findViewById(R.id.et_name);
+        sw_activeCustomer = findViewById(R.id.sw_active);
+        lv_customerList = findViewById(R.id.lv_customerList);
+
+        //Button listeners for the add and view all buttons
+        bt_add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                try {
+                    CustomerModel customerModel = new CustomerModel( -1, et_name.getText().toString(), Integer.parseInt(et_age.getText().toString()), sw_activeCustomer.isChecked());
+                    Toast.makeText(MainActivity.this, "Error creating customer", Toast.LENGTH_SHORT).show();
+                }
+                catch (Exception e){
+
+                }
+
+            }
+        });
+
+        btn_viewAll.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+
     }
 }
